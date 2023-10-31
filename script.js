d4260cadb114bb6c2f932236e3878f5c70523a16
devp2303 <126353525+devp2303@users.noreply.github.com>
2023-10-30 20:41:55 -0400

    Add files via upload
    Java Script Updates (still working on it, changeBackground function in progress)

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-let headerElement = document.getElementById("header");
+let titleElement = document.getElementById("header");
 
 function changeTitle(){
-    titleElement.innerText = "wow hello";
+    titleElement.innerText = "Welcome to my website "+ nameInput.value +"!";
     titleElement.style.color = "red";
+    titleElement.style.fontSize = "xx-large"
 }
-//setTimeout(changeTitle, 1000);
 
-
+function changeBackground(){
+    document.body.style.backgroundImage = "url('img_tree.png')";
+    //document.body.style.backgroundImage = "url('https://1000logos.net/wp-content/uploads/2019/09/Maryland-Terrapins-Logo-1996.png')";
+}
 let clickMeBtn = document.getElementById("clickMe")
-clickMeBtn.addEventListener("click", changeTitle)
+clickMeBtn.addEventListener("click", changeBackground)
+
+const nameInput = document.getElementById("nameInput");
+const greeting = document.getElementById("greeting");
+
+function changeGreeting() {
+    greeting.innerText = nameInput.value;
+}
+
+nameInput.addEventListener("input", changeTitle);
