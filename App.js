342e70198470c76918a5c0811ddc567bd6b3fcb3
devp2303 <126353525+devp2303@users.noreply.github.com>
2023-11-20 18:02:10 -0500

    React Workshop 3 - Routing
    React Workshop 3- Routing

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import Counter from './components/Counter';
-import ProductList from './components/ProductList';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <ProductList />
-      </header>
-    </div>
+    <Router> 
+      <Navbar />
+      <Routes> 
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
